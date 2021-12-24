e2fde313a61502b31971f975f8eb24600607d4e1
William Key <truthseeker009@pm.me>
2021-12-24 16:43:06 +0900

    set template inheritance, began writing DB model

--- venv/market.py
+++ venv/market.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLA_DB_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+class Items(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
 @app.route('/')
-@app.route('/go')
-def go():
+@app.route('/home')
+def home():
     return render_template('bible-study.html')
 
 @app.route('/market')
 def market():
-    return render_template('market.html', item_name='Phone')
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html', items=items)

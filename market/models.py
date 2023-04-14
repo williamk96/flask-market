872a795a831af0c3d4298eb40e87e0ea4d662e57
William Key <truthseeker009@protonmail.com>
2023-04-14 14:41:24 -0400

    add Carts model; add init method for Items; fix table relations

--- market/models.py
+++ market/models.py
@@ -1,4 +1,5 @@
 from market.app import db, bcrypt, login_manager
+from market.lib.get_barcode import barcode
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -6,11 +7,12 @@ def load_user(user_id):
     return Users.query.get(int(user_id))
 
 class Users(db.Model, UserMixin):
+    __tablename__ = 'Users'
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
-    items = db.relationship('Items', backref='owned_user', lazy=True)
+    cart = db.Column(db.Integer(), db.ForeignKey('carts.id'))
 
     @property
     def password(self):
@@ -27,22 +29,33 @@ class Users(db.Model, UserMixin):
         return f'{self.username}'
 
 class Items(db.Model):
+    __tablename__ = 'Items'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
-    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    barcode = db.Column(db.String(length=24), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False)
-    owner = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    inventory = db.Column(db.Integer())
+    in_cart = db.Column(db.Integer(), db.ForeignKey('carts.id'))
 
     def __repr__(self):
         return f'{self.name}'
     
-    def buy(self, user):
-            self.owner = user.id
-            user.budget -= self.price
-            db.session.commit()
-    
-    def sell(self, user):
-        self.owner = None
-        user.budget += self.price
-        db.session.commit()
+#    def buy(self, user):
+#            self.owner = user.id
+#            user.budget -= self.price
+#            db.session.commit()
+
+    def __init__(self, name, price, description):
+        self.name = name
+        self.price = price
+        self.barcode = barcode()
+        self.description = description
+        self.inventory = 0
+
+
+class Carts(db.Model):
+    __tablename__ = 'Carts'
+    id = db.Column(db.Integer(), primary_key=True)
+    user = db.relationship('Users', backref='items', lazy=True)
+    items = db.relationship('Items', backref='in_cart', lazy=True)

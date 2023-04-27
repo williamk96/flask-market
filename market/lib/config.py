c07dc0d264bb756895c0da9e6590cd7f49626961
William Key <truthseeker009@protonmail.com>
2023-04-27 14:02:02 -0400

    Add salt config to config.py and set it to base config

--- market/lib/config.py
+++ market/lib/config.py
@@ -5,29 +5,32 @@ from market.lib.key import key
 path = Path('.')/'.env'
 load_dotenv(dotenv_path=path)
 
-r = key()
+k = key()
 
+# Base configuration
 class config(object):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = k.value
+    SECURITY_PASSWORD_SALT = k.salt
 
+# Base Configuration + Development-specific configuration
 class dev_config(config):
     name = 'development'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///market.db'
     DEBUG = True
     TESTING = True
-    SECRET_KEY = str(r.value)
 
     def __repr__(self):
         return "Development Configuration"
 
 
+# Base Configuration + Production-specific configuration
 class prod_config(config):
     name = 'production'
     # create a production config here
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bsre-one:12qwaszx!@QWASZX'
-    SECRET_KEY = str(r.value)
 
     def __repr__(self):
         return "Production Configuration"

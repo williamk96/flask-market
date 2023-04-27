a1e0f904d37b2cad95e584f8f2d771550069bb0b
William Key <truthseeker009@protonmail.com>
2023-04-27 14:01:58 -0400

    add salt to key for use with hashing algorithms

--- market/lib/key.py
+++ market/lib/key.py
@@ -4,4 +4,5 @@ class key:
 
     uuid = uuid.uuid4()
     value = secrets.token_urlsafe(24)
+    salt = secrets.SystemRandom().getrandbits(128)
     timestamp = datetime.datetime.now(datetime.timezone.utc)

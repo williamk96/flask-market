e6d4feaeefe0256cbc0be0fc1fcb3109ccc17f44
William Key <truthseeker009@protonmail.com>
2023-05-26 21:39:48 -0400

    revise barcode class to conform to barcode lib

--- market/lib/get_barcode.py
+++ market/lib/get_barcode.py
@@ -1,13 +1,21 @@
-import secrets, string
-import code128
+import secrets
+import string
+import barcode
+from barcode.writer import SVGWriter
 from pathlib import Path
 
-path = Path('.')/'market/static/barcodes/'
+path = Path('./market/static/barcodes/')
+#path = Path('.')
 a = string.ascii_letters + string.digits
+
+barcode_format = barcode.get_barcode_class('code128')
 
 class barcode:
     value = ''.join(secrets.choice(a) for i in range(24))
 
-    def image(value):
-        with open('{}.svg'.format(value), 'w') as f:
-            f.write(code128.svg('{}'.format(value)))
+    def __repr__(self):
+        return f'{self.value}'
+
+    def image(self):
+        with open(path/'{}.svg'.format(self.value), 'wb') as f:
+            barcode_format(self.value, writer=SVGWriter()).write(f)
